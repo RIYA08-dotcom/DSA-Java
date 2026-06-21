28e8af4b89275787b6684e127fc2ed0e9f111fc7
RIYA08-dotcom <riyachoudhary9917@gmail.com>
2026-06-22 00:58:34 +0530

    save Find the Difference

---
+++ FindTheDiff.java
@@ -0,0 +1,14 @@
+//leetcode 389-Find the Difference
+class Solution {
+    public char findTheDifference(String s, String t) {
+       char ans=0;
+       for(char ch:s.toCharArray()){
+         ans^=ch;
+       }
+         for(char ch:t.toCharArray()){
+         ans^=ch;
+       }
+       return ans;
+        
+    }
+}
