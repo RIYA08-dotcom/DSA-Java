ca92187c2688a107c2a4b311ac1424d996618574
RIYA08-dotcom <riyachoudhary9917@gmail.com>
2026-07-10 23:56:31 +0530

    Add Set Matrix Zero

---
+++ SetMatrixZero.java
@@ -0,0 +1,39 @@
+/*
+Leetcode 43-Set Matrix Zero
+Time Complexity-O(n^2)
+Space Complexity-O(n)
+Approach-
+1.first find the index of row and column where zero is found.
+2.set zero in that row and column.
+*/
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+       
+        Set<Integer>row=new  HashSet<>();
+        Set<Integer>column=new  HashSet<>();
+
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+            if(matrix[i][j]==0){
+                    row.add(i);
+                    column.add(j);
+                }
+            }
+            } 
+        
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row.contains(i)||column.contains(j)){
+                    matrix[i][j]=0;
+                }
+                
+
+            }
+             
+        }
+        
+    }
+}
